bd5ad5a4e11efbe29395c13d5ae047529e06c194
Rishats <sultanov-rishat@inbox.ru>
2019-06-23 23:36:41 +0600

    Cleaner,Wait until backup finished added.

--- main.go
+++ main.go
@@ -5,14 +5,17 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/getsentry/raven-go"
+	"github.com/h2non/filetype"
 	"github.com/ivahaev/russian-time"
 	"github.com/jasonlvhit/gocron"
 	"github.com/joho/godotenv"
 	"html/template"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"os/exec"
+	"syscall"
 	"time"
 )
 
@@ -122,14 +125,40 @@ func dumpSuccess() {
 	sendToHorn(text)
 }
 
+func cleanerSuccess(fileName string) {
+	type Info struct {
+		FileName string
+	}
+
+	templateData := Info{
+		FileName: fileName,
+	}
+
+	funcmap := template.FuncMap{
+		"weekDay":     weekDay,
+		"hourWithMin": hourWithMin,
+	}
+
+	text, err := getTemplate("successful_cleaner.gohtml", funcmap, templateData)
+	if err != nil {
+		raven.CaptureErrorAndWait(err, nil)
+		log.Panic(err)
+	}
+	sendToHorn(text)
+}
+
+func fileNameGenerate() string {
+	currentTime := time.Now()
+	filename := os.Getenv("MYSQL_DB") + "-" + currentTime.Format("2006_01_02_15_04_05") + ".sql.gz"
+	return filename
+
+}
+
 func mysqlDump() {
-	// mysql_dump
-	cmd := exec.Command("mysqldump",
-		"-P"+os.Getenv("MYSQL_PORT"),
-		"-h"+os.Getenv("MYSQL_HOST"),
-		"-u"+os.Getenv("MYSQL_USER"),
-		"-p"+os.Getenv("MYSQL_PASSWORD"),
-		os.Getenv("MYSQL_DB")+"| gzip > "+os.Getenv("BACKUP_DIR")+os.Getenv("MYSQL_DB")+".$(date +%F.%H%M%S).sql.gz")
+	fileName := fileNameGenerate()
+	cmd := exec.Command("/bin/sh",
+		"-c",
+		"mysqldump "+"-P"+os.Getenv("MYSQL_PORT")+" "+"-h"+os.Getenv("MYSQL_HOST")+" "+"-u"+os.Getenv("MYSQL_USER")+" "+"-p"+os.Getenv("MYSQL_PASSWORD")+" "+os.Getenv("MYSQL_DB")+" | "+"gzip > "+os.Getenv("BACKUP_DIR")+fileName)
 
 	_, err := cmd.StdoutPipe()
 	if err != nil {
@@ -138,7 +167,29 @@ func mysqlDump() {
 		log.Fatal(err)
 	}
 
-	if err := cmd.Start(); err != nil {
+	var waitStatus syscall.WaitStatus
+	if err := cmd.Run(); err != nil {
+		if err != nil {
+			raven.CaptureErrorAndWait(err, nil)
+			dumpError()
+			log.Fatal(err)
+		}
+		if exitError, ok := err.(*exec.ExitError); ok {
+			waitStatus = exitError.Sys().(syscall.WaitStatus)
+			fmt.Printf("Output: %s\n", []byte(fmt.Sprintf("%d", waitStatus.ExitStatus())))
+		}
+	} else {
+		// Success
+		waitStatus = cmd.ProcessState.Sys().(syscall.WaitStatus)
+		fmt.Printf("Output: %s\n", []byte(fmt.Sprintf("%d", waitStatus.ExitStatus())))
+	}
+
+	file := os.Getenv("BACKUP_DIR") + fileName
+	_, err = os.Stat(file)
+
+	// See if the file exists.
+	if os.IsNotExist(err) {
+		raven.CaptureErrorAndWait(err, nil)
 		dumpError()
 		log.Fatal(err)
 	}
@@ -146,11 +197,60 @@ func mysqlDump() {
 	dumpSuccess()
 }
 
+func isOlderThanOneWeek(t time.Time) bool {
+	return time.Now().Sub(t) > 168*time.Hour
+}
+
+func findFilesOlderThanOneWeek(dir string) (files []os.FileInfo, err error) {
+	tmpfiles, err := ioutil.ReadDir(dir)
+	if err != nil {
+		raven.CaptureErrorAndWait(err, nil)
+		log.Fatal(err)
+	}
+
+	for _, file := range tmpfiles {
+		if file.Mode().IsRegular() {
+			if isOlderThanOneWeek(file.ModTime()) {
+				files = append(files, file)
+			}
+		}
+	}
+	return
+}
+
+func gzTypeFileChecking(filename string) string {
+	buf, _ := ioutil.ReadFile(os.Getenv("BACKUP_DIR") + filename)
+
+	kind, _ := filetype.Match(buf)
+	if kind == filetype.Unknown {
+		fmt.Println("Unknown file type")
+		return "unknown"
+	}
+
+	return kind.Extension
+}
+
+func deleteFile(fileName string) {
+	var err = os.Remove(os.Getenv("BACKUP_DIR") + fileName)
+	if err != nil {
+		raven.CaptureErrorAndWait(err, nil)
+		log.Fatal(err)
+	}
+}
+
 func cleaner() {
-	// find old files
-	// delete old file
-	// send to horn if success
-	fmt.Println("cleaner done")
+	files, err := findFilesOlderThanOneWeek(os.Getenv("BACKUP_DIR"))
+	if err != nil {
+		raven.CaptureErrorAndWait(err, nil)
+		log.Fatal(err)
+	}
+	for _, file := range files {
+		fileType := gzTypeFileChecking(file.Name())
+		if fileType == "gz" {
+			deleteFile(file.Name())
+			cleanerSuccess(file.Name())
+		}
+	}
 }
 
 func makeBackup() {
@@ -180,8 +280,6 @@ func main() {
 	if appEnv == "production" {
 		raven.SetDSN(os.Getenv("SENTRY_DSN"))
 	}
-
-	makeBackup()
 
 	tasks()
 }
